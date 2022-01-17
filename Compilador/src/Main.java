423ad7e15f6d0c4e8c5617a08b69c229221e1a54
Beatriz Souza da Silva <beatriz.souza.silva@accenture.com>
2022-01-17 18:52:08 -0300

    Adaptação do main

--- Compilador/src/Main.java
+++ Compilador/src/Main.java
@@ -1,10 +1,11 @@
 import Lexico.Lexer;
+import Sintatico.Syntaxer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // System.out.println(args.toString());
-        Lexer lexer = new Lexer("./Compilador/Testes/teste.txt");
-        lexer.readFile();
-        System.out.println(lexer.getWords().toString());
+        Syntaxer sintatic = new Syntaxer("./Compilador/Testes/teste.txt");
+        sintatic.program();
+        System.out.println("Fim da análise sintática");
     }
 }
